ff7384d162c95bb1d0f07017ad22caed84fd507c
kon <k@smo.la>
2020-11-22 20:02:14 -0800

    more can stuff

--- Makefile
+++ Makefile
@@ -1,8 +1,17 @@
-CC=gcc
-CFLAGS=-I.
+CC = gcc
+CFLAGS = -Wall -g
+INCLUDE = include/smg_tester.h 
+LIBS = -lpthread
+OBJ = smg_tester.o
 
-smg_tester:
-	$(CC) -o smg_tester smg_tester.c
+smg_tester: smg_tester.o
+	$(CC) ${LIBS} ${CFLAGS} -o smg_tester smg_tester.c
 
+smg_tester.o: smg_tester.c include/smg_tester.h
+
+.PHONY: all
+all: smg_tester
+
+.PHONY: clean
 clean: 
-	rm -f smg_tester
+	rm -f smg_tester.o smg_tester
--- smg_tester.c
+++ smg_tester.c
@@ -10,44 +10,72 @@
 
 #include <linux/can.h>
 #include <linux/can/raw.h>
+#include <pthread.h>
+#include "./include/smg_tester.h"
 
-int
-main(void)
+int s;
+struct can_frame frame;
+
+void *canSend(void *vargp)
 {
-	int s;
-	int nbytes;
-	struct sockaddr_can addr;
-	struct can_frame frame;
+
+	while (1)
+	{
+		usleep(TIME_100MS);
+		printf("can thread\n");
+	}
+	return NULL;
+}
+
+int main(void)
+{
+	pthread_t can_thread;
+
+	initialize();
+
+	pthread_create(&can_thread, NULL, canSend, NULL);
+	pthread_join(can_thread, NULL);
+
+	frame.can_id = 0x123;
+	frame.can_dlc = 2;
+	frame.data[0] = 0x11;
+	frame.data[1] = 0x22;
+
+	while (1)
+	{
+		sleep(0.5);
+		printf("main thread\n");
+	}
+
+	return 0;
+}
+
+int initialize()
+{
 	struct ifreq ifr;
+	struct sockaddr_can addr;
 
 	const char *ifname = "vcan0";
 
-	if((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+	if ((s = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0)
+	{
 		perror("Error while opening socket");
 		return -1;
 	}
 
 	strcpy(ifr.ifr_name, ifname);
 	ioctl(s, SIOCGIFINDEX, &ifr);
-	
-	addr.can_family  = AF_CAN;
+
+	addr.can_family = AF_CAN;
 	addr.can_ifindex = ifr.ifr_ifindex;
 
 	printf("%s at index %d\n", ifname, ifr.ifr_ifindex);
 
-	if(bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+	if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0)
+	{
 		perror("Error in socket bind");
 		return -2;
 	}
 
-	frame.can_id  = 0x123;
-	frame.can_dlc = 2;
-	frame.data[0] = 0x11;
-	frame.data[1] = 0x22;
-
-	nbytes = write(s, &frame, sizeof(struct can_frame));
-
-	printf("Wrote %d bytes\n", nbytes);
-	
-	return 0;
+	return 1;
 }
